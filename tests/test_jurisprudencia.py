e8aece5ce8ebdf241e9a8f2eedc1feb1dcbdbd33
danihmorais <67606539+danihmorais@users.noreply.github.com>
2026-09-18 17:25:13 -0300

    Alinha testes ao parser TCESP corrigido

--- tests/test_jurisprudencia.py
+++ tests/test_jurisprudencia.py
@@ -52,7 +52,7 @@ def test_tcu_adapter_uses_current_public_rest_contract():
 def test_tcesp_adapter_parses_result_table():
     html = '''<html><body><table><tr><th>Doc.</th><th>N° Proc.</th><th>Autuação</th><th>Parte 1</th><th>Parte 2</th><th>Matéria</th><th>Objeto</th><th>Exercício</th></tr><tr><td>Relatório / Voto</td><td>5600/989/25</td><td>17/03/2025</td><td>EMPRESA A</td><td>PREFEITURA B</td><td>LICITAÇÃO</td><td>Exame de edital</td><td>2025</td></tr><tr><td colspan="8">Trechos localizados no documento:</td></tr><tr><td colspan="8">A exigência de qualificação técnica deve ser pertinente e proporcional.</td></tr></table></body></html>'''.encode("utf-8")
     records = TCESPAdapter(FakeSession([FakeResponse(html, content_type="text/html", url="https://www.tce.sp.gov.br/jurisprudencia/pesquisar")])).search("licitação", 1)
-    assert len(records) == 1 and records[0].numero_processo == "5600/989/25" and "qualificação técnica" in records[0].ementa
+    assert len(records) == 1 and records[0].numero_processo == "5600/989/25" and records[0].ementa == "A exigência de qualificação técnica deve ser pertinente e proporcional."
 
 
 
@@ -202,7 +202,7 @@ def test_tcm_sp_missing_browser_results_are_reported_as_failure(monkeypatch):
 
 def test_tcesp_missing_results_table_is_reported_as_structure_failure():
     html = "<html><body><p>A página do TCESP foi redesenhada.</p></body></html>".encode("utf-8")
-    with pytest.raises(RuntimeError, match="Estrutura da pesquisa TCESP"):
+    with pytest.raises(RuntimeError, match="Estrutura da pesquisa TCESP|TCESP não retornou resultados estruturados"):
         TCESPAdapter(FakeSession([FakeResponse(html, content_type="text/html", url="https://www.tce.sp.gov.br/jurisprudencia/pesquisar")])).search("licitação", 1)
 
 
