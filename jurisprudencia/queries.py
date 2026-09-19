ae20a43c52b5474396d79dcf5f26bf0d08ce7b96
danihmorais <67606539+danihmorais@users.noreply.github.com>
2026-09-19 11:21:17 -0300

    remove: consulta jurisprudencial à Prefeitura de São Paulo

--- jurisprudencia/queries.py
+++ jurisprudencia/queries.py
@@ -50,7 +50,6 @@ DEFAULT_QUERIES = (
     "governo digital assinatura eletrônica processo eletrônico serviço público",
     "serviço público usuário continuidade universalidade eficiência atendimento",
     "São Paulo processo administrativo Lei 10.177/1998 administração estadual",
-    "São Paulo Município Prefeitura licitação contrato administrativo Decreto 62.100 Lei 14.133",
     "São Paulo Tribunal de Contas controle externo licitação contrato administrativo",
     "TJSP Tribunal de Justiça de São Paulo licitação contrato administrativo Lei 14.133",
     "TJSP licitação edital habilitação contratação pública",
