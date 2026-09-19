35ba85d6906bfdf242937ac6829383ddd0a1d88d
danihmorais <67606539+danihmorais@users.noreply.github.com>
2026-09-19 13:45:09 -0300

    test: enforce historical decree source fallbacks

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -68,6 +68,21 @@ def test_prefeitura_sp_sources_are_absent():
     assert not any(item["id"].startswith("spm-") for item in SOURCES)
 
 
+def test_historical_nllc_value_decrees_use_official_camara_fallback_series():
+    catalog = {item["id"]: item for item in SOURCES}
+    expected = {
+        "decreto10922": "https://www2.camara.leg.br/legin/fed/decret/2021/decreto-10922-30-dezembro-2021-792190-publicacaooriginal-164267-pe.html",
+        "decreto11317": "https://www2.camara.leg.br/legin/fed/decret/2022/decreto-11317-29-dezembro-2022-793592-publicacaooriginal-166702-pe.html",
+        "decreto11871": "https://www2.camara.leg.br/legin/fed/decret/2023/decreto-11871-29-dezembro-2023-795201-publicacaooriginal-170740-pe.html",
+        "decreto12343": "https://www2.camara.leg.br/legin/fed/decret/2024/decreto-12343-30-dezembro-2024-796843-publicacaooriginal-173988-pe.html",
+    }
+    for source_id, url in expected.items():
+        source = catalog[source_id]
+        assert source["urls"] == [url]
+        assert source["fallback_urls"] == (source["fallback_urls"][0],)
+        assert source["fallback_urls"][0].startswith("https://www.planalto.gov.br/")
+
+
 def test_current_source_endpoints():
     catalog = {item["id"]: item for item in SOURCES}
     assert catalog["pncp"]["urls"][0] == "https://www.gov.br/pncp/pt-br/pncp/legislacao"
