e73a99a91b695719f35af6a0904702d5c1565ff7
danihmorais <67606539+danihmorais@users.noreply.github.com>
2026-09-21 10:45:39 -0300

    Restore jurisprudencia/queries.py from conversation recovery

---
+++ jurisprudencia/queries.py
@@ -0,0 +1,15 @@
+QUERIES = {
+    "licitacoes": [
+        "licitação Lei 14.133",
+        "contratação direta dispensa inexigibilidade",
+        "pregão registro de preços",
+        "habilitação proposta julgamento",
+        "sanções impedimento declaração inidoneidade",
+    ],
+    "controle": [
+        "sobrepreço licitação",
+        "pesquisa de preços",
+        "planejamento contratação",
+        "termo de referência estudo técnico preliminar",
+    ],
+}
